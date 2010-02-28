833dd67266bfacdcae72b8c0414dd5412d2a236c
Michael Alexander <beefsack@gmail.com>
2010-02-28 08:36:26 +0000

    More prettying up, added source option.

--- application/controllers/AjaxController.php
+++ application/controllers/AjaxController.php
@@ -3,30 +3,36 @@
 class AjaxController extends Zend_Controller_Action
 {
 
-	public function init()
-	{
-		$this->_helper->layout->disableLayout();
-		$this->_helper->viewRenderer->setNoRender(true);
-	}
-
-	public function fetchdirectoryAction()
-	{
-		$path = str_replace('\\', '/', realpath($this->_getParam('path', dirname(__FILE__))));
-		$directory = array(
+    public function init()
+    {
+    	$this->_helper->layout->disableLayout();
+    	$this->_helper->viewRenderer->setNoRender(true);
+    }
+    
+    public function fetchdirectoryAction()
+    {
+    	$path = str_replace('\\', '/', realpath($this->_getParam('path', dirname(__FILE__))));
+    	$directory = array(
     		'path' => $path,
-		);
-		$files = array();
-		if ($dir = opendir($path)) {
-			while (($file = readdir($dir)) !== false) {
-				$files[] = array(
+    	);
+    	$files = array();
+    	$fileslower = array();
+    	$directories = array();
+    	if ($dir = opendir($path)) {
+    		while (($file = readdir($dir)) !== false) {
+    			$files[] = array(
     				'file' => $file,
     				'directory' => is_dir($path.'/'.$file),
-				);
-			}
-		}
-		$directory['files'] = $files;
-		echo Zend_Json::encode($directory);
-	}
+    			);
+    			$fileslower[] = strtolower($file);
+    			$directories[] = (int) is_dir($path.'/'.$file);
+    		}
+    	}
+    	// Put directories up top, then sort by filename case insensitive
+    	array_multisort($directories, SORT_NUMERIC, SORT_DESC, $fileslower, SORT_STRING, $files);
+    	$directory['files'] = $files;
+    	echo Zend_Json::encode($directory);
+    }
 
 }
 
