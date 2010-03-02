d19a4d94a200f7f862a19d21ae04ed6715f81930
Michael Alexander <beefsack@gmail.com>
2010-03-02 00:59:12 +0000

    Implemented runtime plugin.

---
+++ application/models/LibraryPlugin/Video/Runtime.php
@@ -0,0 +1,16 @@
+<?php
+
+class Model_LibraryPlugin_Video_Runtime extends Model_LibraryPlugin_Video
+{
+	const INCREMENT = 30;
+	
+	public function getStructure(SimpleXMLElement $data)
+	{
+		$structure = array();
+		foreach ($data->item as $item) {
+			$bracket = ((floor(((int) $item->runtime) / self::INCREMENT)) * self::INCREMENT);
+			$structure['By Runtime'][$bracket.' to '.($bracket + self::INCREMENT).' minutes'][$item->title.' ('.$item->year.')'] = (string) $item->path;
+		}
+		return $structure;
+	}
+}
