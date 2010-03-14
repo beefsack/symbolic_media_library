494cc506c9450a23eba0416e394f1f7ec51920ba
Michael Alexander <beefsack@gmail.com>
2010-03-14 06:43:22 +0000

    Trying to fix anime full name listing.

--- application/models/LibraryPlugin/Anime.php
+++ application/models/LibraryPlugin/Anime.php
@@ -5,16 +5,16 @@ abstract class Model_LibraryPlugin_Anime extends Model_LibraryPlugin
 	
 	protected function _parseTitle($title)
 	{
-		// Move the/a to the end of the title
-		if (preg_match('/^(the|a)\s+(.*)/i', $title, $matches)) {
-			$title = $matches[2].', '.$matches[1];
-		}
+//		// Move the/a to the end of the title
+//		if (preg_match('/^(the|a)\s+(.*)/i', $title, $matches)) {
+//			$title = $matches[2].', '.$matches[1];
+//		}
 		return $title;
 	}
 	
 	protected function _buildTitle(SimpleXMLElement $data)
 	{
-		return $this->_parseTitle($data->title->English);
+		return $this->_parseTitle($data->titles->main);
 	}
 	
 	
